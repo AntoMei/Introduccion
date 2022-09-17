5beb4db7ac7576a53fa34f8483facffcc5705498
AntoMei <antonio.meirosu444@gmail.com>
2022-09-17 13:01:14 +0200

    Hecho hasta el ejercicio de array_search

--- arrays.php
+++ arrays.php
@@ -1,8 +1,19 @@
 <?php
-$nombres = array('Antonio', 'Stefan', 'Borja');
+$nombres = array("1." => 'Antonio', "3." => 'Stefan', "2." => 'Borja');
 echo count($nombres) . "<br>";
 
 echo implode(' ', $nombres) . "<br>";
 
-echo asort($nombres);
+asort($nombres);
+foreach ($nombres as $key => $val) {
+    echo "$key = $val\n" . "<br>";
+}
+
+$reversed = array_reverse($nombres);
+print_r($reversed);
+?>
+<br>
+<?php
+$clave = array_search('Antonio', $nombres);
+echo $clave;
 ?>
