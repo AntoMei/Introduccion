babbcbcaa96ee1265a14b96c00121bcdc1ca19b2
AntoMei <antonio.meirosu444@gmail.com>
2022-09-17 13:39:01 +0200

    Finalizado ejercicios de arrays

--- arrays.php
+++ arrays.php
@@ -16,4 +16,43 @@ print_r($reversed);
 <?php
 $clave = array_search('Antonio', $nombres);
 echo $clave;
+
+$alumnos = array(
+    array(
+        'id' => 1,
+        'nombre' => 'Antonio',
+        'edad' => '22',
+    ),
+    array(
+        'id' => 2,
+        'nombre' => 'Stefan',
+        'apellido' => '20',
+    ),
+    array(
+        'id' => 3,
+        'nombre' => 'Borja',
+        'apellido' => '19',
+    ),
+);
+
+$mostrar = '<table border="1">';
+foreach ($alumnos as $key) {
+    $mostrar.='<tr>';
+    foreach ($key as $b) {
+        $mostrar .= '<td>'.$b.'</td>';
+    }
+    $mostrar .= '</tr>';
+}
+$mostrar .= '</table>';
+echo $mostrar;
+?>
+<br>
+<?php
+$columnas = array_column($alumnos, 'nombre');
+print_r($columnas);
+?>
+<br>
+<?php
+$numeros = array(1,2,3,4,5,6,7,8,9,10);
+echo array_sum($numeros);
 ?>
