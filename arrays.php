cc891e0f56920e162143b89e49b9d841d5593151
AntoMei <antonio.meirosu444@gmail.com>
2022-09-19 13:32:53 +0200

    Finalizado Ejercicio arrays

---
+++ arrays.php
@@ -0,0 +1,57 @@
+<?php
+$nombres = array('Antonio', 'Stefan', 'Borja');
+echo count($nombres) . "<br>";
+
+echo implode(' ', $nombres) . "<br>";
+
+$ordenar = $nombres;
+asort($ordenar);
+echo implode(" ", $ordenar) . "<br>";
+
+$reversed = array_reverse($nombres);
+print_r($reversed);
+?>
+<br>
+<?php
+$clave = array_search('Antonio', $nombres);
+echo $clave;
+
+$alumnos = array(
+    array(
+        'id' => 1,
+        'nombre' => 'Antonio',
+        'edad' => '22',
+    ),
+    array(
+        'id' => 2,
+        'nombre' => 'Stefan',
+        'apellido' => '20',
+    ),
+    array(
+        'id' => 3,
+        'nombre' => 'Borja',
+        'apellido' => '19',
+    ),
+);
+
+$mostrar = '<table border="1">';
+foreach ($alumnos as $key) {
+    $mostrar.='<tr>';
+    foreach ($key as $b) {
+        $mostrar .= '<td>'.$b.'</td>';
+    }
+    $mostrar .= '</tr>';
+}
+$mostrar .= '</table>';
+echo $mostrar;
+?>
+<br>
+<?php
+$columnas = array_column($alumnos, 'nombre');
+print_r($columnas);
+?>
+<br>
+<?php
+$numeros = array(1,2,3,4,5,6,7,8,9,10);
+echo array_sum($numeros);
+?>
