b1f46afe4263e2b3d3f874d88cbe8ac3694e9981
AntoMei <antonio.meirosu444@gmail.com>
2022-09-21 10:28:00 +0200

    Finalizado Ejercicio callback

--- callback.php
+++ callback.php
@@ -1,6 +1,6 @@
 <?php
     $cadenas = array("hola que tal", "buenos dias buenas noches", "hola");
-    $longitud = array_map('strlen', $cadenas);
+    $longitud = array_map("strlen", $cadenas);
     print_r($longitud);
-    echo ".<br>" + max($longitud);
+    echo "<br>" . max($longitud) . " "  . min($longitud);
 ?>
