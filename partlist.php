8dc2157940c14dd5c4740dc955ad859a0a603505
AntoMei <antonio.meirosu444@gmail.com>
2022-09-21 17:32:19 +0200

    Finalizado Ejercicio partlist

--- partlist.php
+++ partlist.php
@@ -1,4 +1,15 @@
 <?php
-$cadenas = array("Seguro", "que", "apruebo", "esta", "asignatura");
+$cadenas = array("Hola", "que", "tal", "estas");
 
+for ($i=0; $i < count($cadenas); $i++) { 
+    for ($j=0; $j < count($cadenas); $j++) { 
+        $elemento1 = array_slice($cadenas, 0, $i);
+        $elemento2 = array_slice($cadenas, $i, $j);
+
+        print_r($elemento1);
+        echo "<br>";
+        print_r($elemento2);
+        echo "<br>";
+    }
+}
 ?>
