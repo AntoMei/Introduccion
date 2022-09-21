d8cadc4b408da47de15d5734ad8429327d8f7534
AntoMei <antonio.meirosu444@gmail.com>
2022-09-21 10:26:07 +0200

    Finalizado Ejercicio sort2

---
+++ callback.php
@@ -0,0 +1,6 @@
+<?php
+    $cadenas = array("hola que tal", "buenos dias buenas noches", "hola");
+    $longitud = array_map('strlen', $cadenas);
+    print_r($longitud);
+    echo ".<br>" + max($longitud);
+?>
--- sort2.php
+++ sort2.php
@@ -1,9 +1,27 @@
 <?php
- $temperaturas = [21, 32, 17, 14, 20, 25, 19, 39, 8, 16];
- $cadena = implode(" ",$temperaturas);
- explode(" ", $cadena);
- echo count($temperaturas);
- 
- $temperaturaBAJA = 0;
- $temperaturaALTA = 0; 
+$temperaturas = "34 40.2 22.4 12 35.6 14 15 16";
+$temperaturasDelMes = explode(" ", $temperaturas);
+
+$media = array_sum($temperaturasDelMes) / count($temperaturasDelMes);
+
+$temperaturaAsc = $temperaturasDelMes;
+asort($temperaturaAsc);
+
+$temperaturaDesc = $temperaturasDelMes;
+arsort($temperaturaDesc);
+
+echo("Todas las temperaturas: ");
+foreach($temperaturasDelMes as $temperatura){
+    echo "$temperatura ";
+}
+
+echo "<br> Temperaturas maximas: ";
+print_r(array_slice($temperaturaDesc, 0, 5));
+
+echo "<br> Temperaturas minimas: ";
+for($i=0; $i < 5; $i++){
+    echo "$temperaturaAsc[$i] ";
+}
+
+echo "<br>Temperatura media de los meses: $media <br>";
 ?>
