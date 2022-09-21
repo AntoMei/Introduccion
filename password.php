959827aa3f2224af7580b7aae0fcaa35b7398196
AntoMei <antonio.meirosu444@gmail.com>
2022-09-21 10:03:27 +0200

    Finalizado Ejercicio password

--- password.php
+++ password.php
@@ -1,22 +1,27 @@
 <?php
 function rand_Pass($upper = 1, $lower = 5, $numeric = 3, $other = 2){
 
-    $numeros = array (134,43,542,5432,6745);
-
+    $numeros = array();
     for ($i=0; $i < $upper; $i++) {
-        $upper = chr(rand(65, 90));
+        array_push($numeros, chr(rand(65, 90)));
     }
 
     for ($i=0; $i < $lower; $i++) { 
-        $lower = chr(rand(65, 90));
+        array_push($numeros, chr(rand(97, 122)));
     }
 
     for ($i=0; $i < $numeric; $i++) { 
-        $numeric = chr(rand(65, 90));
+        array_push($numeros, chr(rand(48, 57)));
     }
 
     for ($i=0; $i < $other; $i++) { 
-        $other = chr(rand(65, 90));
+        array_push($numeros, chr(rand(219, 223)));
+    }
+    
+    shuffle($numeros);
+    foreach ($numeros as $numero) {
+        echo "$numero";
     }
 }
+rand_Pass();
 ?>
