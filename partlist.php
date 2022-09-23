b118754b251a53cfd01b56208d51d35404298ad7
AntoMei <antonio.meirosu444@gmail.com>
2022-09-23 11:42:09 +0200

    Finalizado Ejercicio partlist

--- partlist.php
+++ partlist.php
@@ -1,10 +1,16 @@
 <?php
 $cadenas = array("Hola", "que", "tal", "estas");
+$resultado = [];
 
-for ($i=0; $i < count($cadenas); $i++) { 
-        $elemento1 = array_slice($cadenas, 0, $i);
+for ($i=0; $i < count($cadenas) - 1; $i++) { 
 
-        echo implode($elemento1);
-        echo "<br>";;
+        $elemento1 = array_slice($cadenas, 0, $i+1);
+        $elemento2 = array_slice($cadenas, $i+1);
+
+        $elementoFinal = [];
+        $elementoFinal[] = implode(" ", $elemento1);
+        $elementoFinal[] = implode(" ", $elemento2);
+        $resultado[] = $elementoFinal;
     }
+print_r($resultado);
 ?>
