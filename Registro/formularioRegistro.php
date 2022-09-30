420075b8b6242d34549553af913800ff6214186b
AntoMei <antonio.meirosu444@gmail.com>
2022-09-30 12:05:15 +0200

    Creado la carpeta Registro

---
+++ Registro/formularioRegistro.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<form method="POST" enctype="multipart/form-data">
+    Nombre de usuari@ <input type="text" name="nombre"><br><br>
+    Correo Electrónico <input type="email" name="email"><br><br>
+    Contraseña <input type="email" name="email"><br><br>
+    Confirma la contraseña <input type="email" name="email"><br><br>
+    <input type="submit" value="Registrase">
+    <br><br>
+    ¿Ya eres miembro? <a href="">Acceso usuari@s</a>
+</form>
+</body>
+</html>
+
+<?php
+?>
---
+++ Registro/loginRegistro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<form method="POST" enctype="multipart/form-data">
+    Nombre de usuari@ <input type="text" name="nombre"><br><br>
+    Contraseña: <input type="email" name="email"><br><br>
+    <input type="submit" value="Acceder">
+    <br><br>
+    ¿Todavía no eres miembro? <a href="">Registrarse</a>
+</form>
+</body>
+</html>
